0552b34851d318421d754290fd52fc896f67581e
HadyMohamedMorsy <hady912012@gmail.com>
2023-02-21 15:20:59 +0200

    put menubar

--- src/app/pages/auth/components-auth/login/login.component.ts
+++ src/app/pages/auth/components-auth/login/login.component.ts
@@ -1,58 +1,69 @@
 import { Router } from '@angular/router';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { Login } from '@modal/login';
 import { AuthService } from '@services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {MessageService} from 'primeng/api';
 import { NgxSpinnerService } from "ngx-spinner";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  providers: [MessageService]
+  providers: [MessageService],
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnDestroy {
   private Router = inject(Router);
   private MessageService = inject(MessageService);
   private loading = inject(NgxSpinnerService);
-  private auth  = inject(AuthService);
+  private auth = inject(AuthService);
   private LoginBuilder = inject(FormBuilder);
-  contactLoginForm !: FormGroup;
+  private sub$ !: Subscription;
+  contactLoginForm!: FormGroup;
 
   ngOnInit(): void {
     this.contactLoginForm = this.LoginBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
-    })
+    });
   }
 
   onSubmit() {
-    if(
-    this.contactLoginForm.controls['email'].status == 'INVALID'
-    || this.contactLoginForm.controls['email'].errors != null
-    || this.contactLoginForm.controls['password'].status == 'INVALID'
-    ){
+    if (
+      this.contactLoginForm.controls['email'].status == 'INVALID' ||
+      this.contactLoginForm.controls['email'].errors != null ||
+      this.contactLoginForm.controls['password'].status == 'INVALID'
+    ) {
       this.loading.show();
       setTimeout(() => {
-        this.MessageService.add({ severity: 'error', summary: 'please check your Email Or Password', detail: 'Validation failed'});
+        this.MessageService.add({
+          severity: 'error',
+          summary: 'please check your Email Or Password',
+          detail: 'Validation failed',
+        });
         this.loading.hide();
       }, 1500);
-
-    }else{
+    } else {
       this.loading.show();
-      this.auth.login(this.contactLoginForm.value)
-      .subscribe({
+      this.sub$ = this.auth.login(this.contactLoginForm.value).subscribe({
         next: () => {
           this.loading.hide();
           this.Router.navigate(['/']);
         },
         error: (e) => {
           this.loading.hide();
-          this.MessageService.add({ severity: 'error', summary: e, detail: 'Validation failed' });
-        }
-      })
+          this.MessageService.add({
+            severity: 'error',
+            summary: e,
+            detail: 'Validation failed',
+          });
+        },
+      });
     }
+  }
+
+  ngOnDestroy() {
+    this.sub$.unsubscribe();
   }
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './components-dashboard/dashboard/dashboard.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [
-    DashboardComponent
-  ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+  declarations: [DashboardComponent],
+  imports: [CommonModule, DashboardRoutingModule, SharedModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
